d7275ecec0b4e210bb2c1ea9cc7e63b353b57136
Jeremy Larkin <jeremy@imgix.com>
2017-07-14 11:47:10 -0700

    remove tolower

--- lib/util.c
+++ lib/util.c
@@ -6,12 +6,12 @@ ed_parse_size(const char *val, long long *out)
 	char *end;
 	long long size = strtoll(val, &end, 10);
 	if (size < 0) { return false; }
-	switch(tolower(*end)) {
-	case 'k': size *= ED_KiB; end++; break;
-	case 'm': size *= ED_MiB; end++; break;
-	case 'g': size *= ED_GiB; end++; break;
-	case 't': size *= ED_TiB; end++; break;
-	case 'p': size *= PAGESIZE; end++; break;
+	switch(*end) {
+	case 'k': case 'K': size *= ED_KiB; end++; break;
+	case 'm': case 'M': size *= ED_MiB; end++; break;
+	case 'g': case 'G': size *= ED_GiB; end++; break;
+	case 't': case 'T': size *= ED_TiB; end++; break;
+	case 'p': case 'P': size *= PAGESIZE; end++; break;
 	}
 	if (*end != '\0') { return false; }
 	*out = size;
