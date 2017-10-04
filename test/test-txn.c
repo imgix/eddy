90c2d340523f93907351c57fe7d210c2cf8eb1bf
Jeremy Larkin <jeremy@imgix.com>
2017-10-04 12:11:13 -0700

    Revert "cleanup before forking"
    This reverts commit dbe0d49e5a20f71e2bc73e2973e2699de1ea0cb1.

--- test/test-txn.c
+++ test/test-txn.c
@@ -178,6 +178,7 @@ test_read_snapshot(void)
 	unlink(cfg.index_path);
 
 	EdTxn *txn;
+	setup(&txn);
 
 	Entry ent;
 
@@ -195,9 +196,6 @@ test_read_snapshot(void)
 	snprintf(ent.name, sizeof(ent.name), "a%" PRIu64, ent.key);
 	mu_assert_int_eq(ed_bpt_find(txn, 0, ent.key, NULL), 0);
 	mu_assert_int_eq(ed_bpt_set(txn, 0, &ent, false), 0);
-
-	ed_txn_close(&txn, FRESET);
-	finish(&txn);
 
 	pid_t pid = fork();
 	if (pid < 0) {
@@ -231,7 +229,6 @@ test_read_snapshot(void)
 #endif
 	}
 	else {
-		setup(&txn);
 		sleep(1);
 
 		mu_assert_int_eq(ed_txn_commit(&txn, FRESET), 0);
@@ -264,8 +261,6 @@ test_write_sequence(void)
 	mu_assert_int_eq(ed_bpt_set(txn, 0, &ent, false), 0);
 	mu_assert_int_eq(ed_txn_commit(&txn, FRESET), 0);
 
-	finish(&txn);
-
 	pid_t pid = fork();
 	if (pid < 0) {
 		mu_fail("fork failed '%s'\n", strerror(errno));
@@ -291,7 +286,6 @@ test_write_sequence(void)
 	}
 	else {
 		mu_assert_int_eq(ed_txn_open(txn, FOPEN), 0);
-		setup(&txn);
 
 		sleep(1);
 
